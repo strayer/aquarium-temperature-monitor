d5e82bd3c1983d484819d3362e0735ad31140aef
Sven Grunewaldt <strayer@olle-orks.org>
2018-08-28 11:51:18 +0200

    Send a nice User-Agent to InfluxDB

--- apps/aquarium_temperature_monitor/lib/aquarium_temperature_monitor/temperature_reporter/influxdb_handler.ex
+++ apps/aquarium_temperature_monitor/lib/aquarium_temperature_monitor/temperature_reporter/influxdb_handler.ex
@@ -1,6 +1,11 @@
 defmodule AquariumTemperatureMonitor.TemperatureReporter.InfluxDBHandler do
   alias AquariumTemperatureMonitor.TemperatureMonitor.TemperatureReading
 
+  @headers [
+    "User-Agent":
+      "#{Atom.to_string(:aquarium_temperature_monitor)}/#{Mix.Project.config()[:version]}"
+  ]
+
   def send_temperature(%TemperatureReading{} = reading, config) do
     {:ok, %HTTPoison.Response{status_code: 204}} =
       reading
@@ -16,7 +21,7 @@ defmodule AquariumTemperatureMonitor.TemperatureReporter.InfluxDBHandler do
 
   defp send_data(data, config) do
     url = String.trim_trailing(config[:url], "/")
-    HTTPoison.post("#{url}/write?db=#{URI.encode(config[:db])}", data)
+    HTTPoison.post("#{url}/write?db=#{URI.encode(config[:db])}", data, @headers)
   end
 
   defp format_datetime(datetime) do
